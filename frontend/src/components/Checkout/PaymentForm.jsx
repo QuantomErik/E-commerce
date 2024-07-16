580cc46518223950675b88e73e390f3d70de2575
QuantomErik <ey222ci@student.lnu.se>
2024-07-16 16:22:01 +0200

    Change url for create-payment-intent for deployment.

--- frontend/src/components/Checkout/PaymentForm.jsx
+++ frontend/src/components/Checkout/PaymentForm.jsx
@@ -46,7 +46,8 @@ const PaymentForm = ({ amount, cartItems }) => {
 
     console.log('Payment method created:', paymentMethod);
 
-    const response = await fetch('http://127.0.0.1:8000/api/create-payment-intent/', {
+    /* const response = await fetch('http://127.0.0.1:8000/api/create-payment-intent/', { */
+    const response = await fetch('http://127.0.0.1:8000/ecommerce/api/create-payment-intent/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
