5d4028dd064b46fd2ccda5dbcfbb94f7be3f2f25
QuantomErik <ey222ci@student.lnu.se>
2024-07-16 16:15:36 +0200

    Correct spelling.

--- backend/ecommerce_site/urls.py
+++ backend/ecommerce_site/urls.py
@@ -58,7 +58,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-     urlpatterns += [re_path(r'^ecommerce/.*$', TemplateView.as_view(template_name='index.html'))
+    urlpatterns += [re_path(r'^ecommerce/.*$', TemplateView.as_view(template_name='index.html'))
 ]  
 
 
