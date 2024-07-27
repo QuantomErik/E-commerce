5e56a3bcc170cbd0dcf6e79539ab176aa6a6208f
QuantomErik <ey222ci@student.lnu.se>
2024-07-27 13:59:23 +0200

    Change token refresh.

--- frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -64,7 +64,7 @@ export default ProtectedRoute; */
 
 import { Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import api from "../../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../../constants";
 
